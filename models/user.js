7aec6529bb822e743c31299fae6e2f5eac7723f9
Greg Bastianelli <greg@gerrg.com>
2020-10-23 22:40:48 -0400

    add sign up date to user model

--- models/user.js
+++ models/user.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
   },
   passwordHash: { type: String, required: true },
   name: { type: String, required: true },
+  date: { type: Date, required: true },
 });
 
 userSchema.set("toJSON", {
